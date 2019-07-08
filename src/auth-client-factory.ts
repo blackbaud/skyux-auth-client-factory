a24c23461579f1599e047a8cabc087d39d9097b1
Paul Crowder <paul.crowder@blackbaud.com>
2019-07-08 10:35:48 -0400

    Initial commit (#1)
    * Initial commit
    
    * Remove codelyzer rules
    
    * noop test script
    
    * Create .travis.yml

---
+++ src/auth-client-factory.ts
@@ -0,0 +1,27 @@
+import {
+  BBAuth,
+  BBContextProvider,
+  BBOmnibar
+} from '@blackbaud/auth-client';
+
+function getGlobal(name: string): any {
+  const windowRef: any = window;
+
+  return windowRef.BBAuthClient && windowRef.BBAuthClient[name];
+}
+
+export class BBAuthClientFactory {
+
+  public static get BBAuth(): typeof BBAuth {
+    return getGlobal('BBAuth') || BBAuth;
+  }
+
+  public static get BBContextProvider(): typeof BBContextProvider {
+    return getGlobal('BBContextProvider') || BBContextProvider;
+  }
+
+  public static get BBOmnibar(): typeof BBOmnibar {
+    return getGlobal('BBOmnibar') || BBOmnibar;
+  }
+
+}
